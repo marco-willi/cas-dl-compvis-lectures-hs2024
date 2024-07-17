463cbd3283b1db0659b255e3e56cc6e192f0cb23
Marco Willi <marco.willi@fhnw.ch>
2024-07-17 17:59:01 +0000

    Built site for gh-pages

--- move.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SOURCE=/d/Lehre/BSc_Informatik_bverI/hs2023/code/bveri-lectures-hs2023/book/_build/html
-TARGET=/d/Lehre/BSc_Informatik_bverI/hs2023/code/bveri-lectures-hs2023-branch-gh-pages/html
-
-rm -rf ${TARGET}
-
-cp -r ${SOURCE} ${TARGET}
